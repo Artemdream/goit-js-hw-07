7a76f10c4b6c8c2373154e24bf433d7beb33a5a2
Artem <113770041+Artemdream@users.noreply.github.com>
2023-01-19 22:03:40 +0200

    home work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const container = document.querySelector('.gallery');
+const gallaryMarkup = createGallery(galleryItems); 
+
+
+container.insertAdjacentHTML('beforeend', gallaryMarkup)
+
+container.addEventListener('click', onClick);
+
+function createGallery (images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+           />
+          </a>
+        </div> 
+        `;
+    }).join(""); 
+} 
+
+function onClick(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return
+     };
+
+    const instance = basicLightbox.create(`
+    <img
+    src="${e.target.dataset.source}"
+    />
+    `);  
+
+    instance.show()
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 console.log(galleryItems);
+
+
+const container = document.querySelector('.gallery');
+container.classList.add('gallery__item');
+const gallaryMarkup = createGallery(galleryItems); 
+container.insertAdjacentHTML('beforeend', gallaryMarkup);
+
+function createGallery (images) {
+    return images.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}"/>
+        </a>
+        `;
+    }).join(""); 
+};
+
+container.addEventListener('click', onClick);
+
+function onClick(e) {
+    e.preventDefault()
+    if (e.target.nodeName !== 'IMG') {
+        return
+     }; 
+
+     e.target.getAttribute("alt")
+     
+};
+ new SimpleLightbox('.gallery a', 
+ {captionDelay: 250, captionPosition: 'bottom', captionsData: 'alt'});
